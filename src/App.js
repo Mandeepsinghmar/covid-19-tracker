22b895cd0de22fd00a660d0a447943cfc7c14a7d
Mandeep Singhmar <msigh8328@gmail.com>
2021-02-19 16:50:40 +0530

    make some change in dropdown

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-
+import react, { useState, useEffect } from 'react';
+import { Select, MenuItem, FormControl } from '@material-ui/core'
 import './App.css';
 
 function App() {
+const [ countries, setCountries] = useState([]);
+const [ country, setCountry] = useState(["Worldwide"]);
 
+useEffect(() => {
+
+ const countriesData = async () => {
+
+  await fetch('https://disease.sh/v3/covid-19/countries')
+ .then(response => response.json())
+ .then((data)  => {
+   const countries = data.map((country) => (
+     {
+     name: country.country,
+     value: country.countryInfo.iso2,
+   }
+   ));
+   setCountries(countries)
+ });
+ };
+ countriesData();
+ 
+}, [])
 
   return (
     <div className="App">
 
-
       <div className="app__header">
              {/* header */}
              <h2>covid-19 tracker</h2>
+
             {/* select countries */}
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} >
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+            {/* loop through all the countries and show a dropdown menu */}
+           {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+           ))}
+            
+          </Select>
+
+        </FormControl>
+            
       </div>
 
       <div className="app__infobox">
